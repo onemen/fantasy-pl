c7bf5c6650400562c162bfb524abb80dabe0f150
onemen <tabmix.onemen@gmail.com>
2020-09-25 13:17:14 +0300

    use react-markdown to render blog clean up styles, reforamt remove new line and sapces from md file

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,17 +1,12 @@
 import { css } from '@emotion/core';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
-import React from 'react';
 import Markdown from 'react-markdown';
-import rehypeReact from 'rehype-react';
+import ReactMarkdown from 'react-markdown/with-html';
 import InfoLine from '../components/infoLine';
 import Layout from '../components/layout';
 import format from '../lib/format';
 import theme from '../styles/theme';
-
-const renderAst = new rehypeReact({
-  createElement: React.createElement,
-}).Compiler;
 
 const blogStyle = css`
   width: 100%;
@@ -20,65 +15,89 @@ const blogStyle = css`
   padding: 20px 40px 40px;
   background-color: ${theme.colors.darkGray};
 
-  h2 {
-    margin-top: 2.55rem;
-    margin-bottom: 1.55rem;
+  h1 {
+    font-size: 2.25rem;
+    line-height: 1.1;
+    margin-bottom: 1rem;
+    text-align: center;
   }
 
-  h3 {
-    margin-top: 0;
-    margin-bottom: 0;
+  .banner {
+    text-align: center;
+    margin: 1rem auto;
+    p {
+      margin-bottom: 0;
+    }
+    @media (max-width: 767px) {
+      padding: 0;
+    }
+
+    img {
+      max-height: 400px;
+    }
   }
 
-  p {
-    margin: 0 0 1.55rem 0;
-  }
+  .blog {
+    h2 {
+      margin-top: 2.55rem;
+      margin-bottom: 1.55rem;
+    }
 
-  p + ul {
-    margin-top: calc(-1.55rem / 1.5);
-  }
+    h3 {
+      margin-top: 0;
+      margin-bottom: 0;
+    }
 
-  ul {
-    margin-block-start: 1em;
-    margin-block-end: 1em;
-    padding-inline-start: 40px;
-  }
+    p {
+      margin: 0 0 1.55rem 0;
+    }
 
-  table {
-    font-size: 0.875rem;
-    border-collapse: collapse;
-    border-spacing: 0;
-    direction: rtl;
-    margin: 0 auto;
-  }
-  tbody {
-    display: table-row-group;
-    vertical-align: middle;
-  }
-  tr {
-    display: table-row;
-    vertical-align: inherit;
-  }
-  th {
-    padding-top: 11px;
-    padding-bottom: 11px;
-    background-color: ${theme.brand.primary};
-    color: white;
-  }
-  td,
-  th {
-    border: 1px solid #ddd;
-    text-align: left;
-    padding: 8px;
-  }
-  tr:nth-of-type(2n) {
-    background-color: ${theme.colors.primaryDark};
-  }
-  table p {
-    margin: 0;
-    text-align: right;
-    margin-inline-start: 0.75rem;
-    margin-inline-end: 0.75rem;
+    p + ul {
+      margin-top: calc(-1.55rem / 1.5);
+    }
+
+    ul {
+      margin-block-start: 1em;
+      margin-block-end: 1em;
+      padding-inline-start: 40px;
+    }
+
+    table {
+      font-size: 0.875rem;
+      border-collapse: collapse;
+      border-spacing: 0;
+      direction: rtl;
+      margin: 0 auto 1.55rem;
+    }
+    tbody {
+      display: table-row-group;
+      vertical-align: middle;
+    }
+    tr {
+      display: table-row;
+      vertical-align: inherit;
+    }
+    th {
+      padding-top: 11px;
+      padding-bottom: 11px;
+      background-color: ${theme.brand.primary};
+      color: white;
+    }
+    td,
+    th {
+      border: 1px solid #ddd;
+      text-align: left;
+      padding: 8px;
+    }
+    tr:nth-of-type(2n) {
+      background-color: ${theme.colors.primaryDark};
+    }
+    table p {
+      margin: 0;
+      text-align: right;
+      margin-inline-start: 0.75rem;
+      margin-inline-end: 0.75rem;
+    }
   }
 `;
 
@@ -93,16 +112,7 @@ const blog = ({ data, ...rest }) => {
 
   return (
     <Layout dir="rtl" css={blogStyle} frontmatter={data.markdownRemark.fields}>
-      <h1
-        css={css`
-          font-size: 2.25rem;
-          line-height: 1.1;
-          margin-bottom: 1rem;
-          text-align: center;
-        `}
-      >
-        {title}
-      </h1>
+      <h1>{title}</h1>
 
       <InfoLine size="">
         <span>{author}</span>
@@ -110,31 +120,15 @@ const blog = ({ data, ...rest }) => {
       </InfoLine>
 
       {banner && (
-        <div
-          css={css`
-            text-align: center;
-            margin: 1rem auto;
-            p {
-              margin-bottom: 0;
-            }
-            @media (max-width: 767px) {
-              padding: 0;
-            }
-          `}
-        >
-          <Img
-            css={{ maxHeight: '400px' }}
-            fluid={banner.childImageSharp.fluid}
-          />
+        <div className="banner">
+          <Img fluid={banner.childImageSharp.fluid} />
           {bannerCredit ? <Markdown>{bannerCredit}</Markdown> : null}
         </div>
       )}
 
-      {/*       <article
-        dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
-      ></article> */}
-
-      <article>{renderAst(data.markdownRemark.htmlAst)}</article>
+      <article className="blog">
+        <ReactMarkdown source={data.markdownRemark.html} escapeHtml={false} />
+      </article>
     </Layout>
   );
 };
@@ -153,7 +147,19 @@ export const pageQuery = graphql`
         }
         bannerCredit
       }
+      html
       htmlAst
     }
   }
 `;
+
+/*
+clean html:
+1. remove all new line /\n/g -> ''
+2. remove all spaces between >< />\s+</g -> '><'
+3. remove all space after <em > /<em\s+>/g -> '<em>'
+4. remove all space after </em > /</em\s+>/g -> '<em>'
+5. replace all spaces with on space ??
+
+
+*/
