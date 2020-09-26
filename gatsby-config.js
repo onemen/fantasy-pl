b623193d93c55fd5d4ee2d136274fd44108ee7f7
onemen <tabmix.onemen@gmail.com>
2020-09-26 07:14:59 +0300

    more plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,16 @@ module.exports = {
     },
     'gatsby-plugin-emotion',
     'gatsby-plugin-pnpm',
+    'gatsby-plugin-catch-links',
     'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        color: config.themeColor,
+        showSpinner: false,
+      },
+    },
+    'gatsby-plugin-remove-serviceworker',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
@@ -77,6 +86,13 @@ module.exports = {
         excerpt: true,
         excerpt_separator: `<!-- end -->`,
         plugins: [
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+          {
+            resolve: 'gatsby-remark-autolink-headers',
+            icon: `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z"/></svg>`,
+            enableCustomId: true,
+          },
           // gatsby-remark-relative-images must go before gatsby-remark-images
           'gatsby-remark-relative-images',
           {
@@ -92,14 +108,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'פנטזי ליג בלוג',
-        short_name: 'פנטזי ליג',
-        description:
-          'בואו לבדוק את פנטזי ליג בלוג, האתר שיגלה לכם כל מה שאתם לא יודעים',
-        start_url: '/',
-        lang: 'he',
-        background_color: '#575353',
-        theme_color: '#00CA6F',
+        name: config.siteTitle,
+        short_name: config.siteTitleShort,
+        description: config.siteDescription,
+        start_url: config.pathPrefix,
+        lang: config.lang,
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
         display: 'standalone',
         icon: 'src/images/icon.png',
         icons: [
@@ -116,5 +131,41 @@ module.exports = {
         ],
       },
     },
+    /**
+     *  TODO:
+     *
+     *  enable when we get a domain name
+     *
+     *  pnpm install gatsby-plugin-robots-txt
+     *
+     {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{userAgent: '*'}],
+          },
+          'branch-deploy': {
+            policy: [{userAgent: '*', disallow: ['/']}],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{userAgent: '*', disallow: ['/']}],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
+     */
+    'gatsby-plugin-sitemap',
+    /**
+     *  TODO:
+     *
+     *  read more about typography for hebrew font
+     *  gatsby-plugin-typography
+     */
   ],
 };
